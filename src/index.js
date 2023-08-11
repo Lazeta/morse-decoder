e3df5fc8ec703edd39495247a18dad1f639edf89
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-08-11 23:35:12 +0300

    feat: add solution for task #4

--- src/index.js
+++ src/index.js
@@ -39,10 +39,10 @@ const MORSE_TABLE = {
 
 function decode(bits) {
     console.log(bits)
-    // 1. Проверка и дополнение нулями
-    const length = bits.length;
-    const paddingLeft = length % 10 === 0 ? '' : 10 - (length % 10);
-    bits = "0".repeat(paddingLeft) + bits;
+    // // 1. Проверка и дополнение нулями
+    // const length = bits.length;
+    // const paddingLeft = length % 10 === 0 ? '' : 10 - (length % 10);
+    // bits = "0".repeat(paddingLeft) + bits;
 
     // 2. Разбиение строки на группы по 10 символов
     let newStr = bits.match(/.{1,10}/g).join(" ");
@@ -51,22 +51,28 @@ function decode(bits) {
     // 3. Декодирование
     let decodeStr = "";
     for(let i = 0; i < newStr.length; i++){
-        if(newStr[i] === " "){ 
+        if(newStr.substring(i, i+10)  === "**********"){ 
+            decodeStr += "*";
+        }
+        if(newStr[i] === " "){
             decodeStr += " ";
         }
-        if(newStr[i] == 1 && newStr[i+1] == 0 && newStr[i+2] != 0){
-            decodeStr += ".";
-        }
-        if(newStr[i] == 1 && newStr[i+1] == 1 && newStr[i+2] != 0){
+        if(newStr.substring(i, i+1) === "11"){
             decodeStr += "-";
+        }
+        if(newStr.substring(i, i+1) === "10"){
+            decodeStr += ".";
         }
     }
     
     // 4. Разбиваем на массив
-    decodeStr = decodeStr.split("  ");
+    let decodeWord = decodeStr.split("*");
+    let letters = decodeWord.join('').split(" ")
+    // console.log(decodeWord)
+    // console.log(letters)
 
     // 5. 
-    const result = decodeStr.map(word => {
+    const result = decodeWord.map(word => {
         if (word === "") {
             return " ";
         }
@@ -77,10 +83,11 @@ function decode(bits) {
     return result
 }
 
-console.log(decode("00000011110000000010"))
-console.log(decode("000000111100000000000000001111"))
+// console.log(decode("00000011110000000010"))
+console.log(decode("0000001111**********0000001111"))
 // console.log(decode("000000111100000000100000000000000000011110000000010"))
 // console.log(decode("00000011110000000010000000111000111000001110001110111000000000000111011101110001110111011100000000001110111"))
+console.log(decode("0000101110000011111100101110100010111010000000101000000011100000111110**********00001010100011101110000011111100101111100000000010**********000010101000111011100010101010000011111100001111110010111010"))
 
 
 // module.exports = {
