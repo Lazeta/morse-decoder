69fcf93edf8f58d1a28f8a349159ec5d4f8f4f12
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-08-12 10:21:10 +0300

    feat: add solution to task #5

--- src/index.js
+++ src/index.js
@@ -38,38 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(bits) {
-    console.log(bits)
-    // // 1. Проверка и дополнение нулями
-    // const length = bits.length;
-    // const paddingLeft = length % 10 === 0 ? '' : 10 - (length % 10);
-    // bits = "0".repeat(paddingLeft) + bits;
+    // 1. Проверка и дополнение нулями
+    const length = bits.length;
+    const paddingLeft = length % 10 === 0 ? '' : 10 - (length % 10);
+    bits = "0".repeat(paddingLeft) + bits;
 
     // 2. Разбиение строки на группы по 10 символов
     let newStr = bits.match(/.{1,10}/g).join(" ");
-    console.log(newStr)
 
     // 3. Декодирование
     let decodeStr = "";
     for(let i = 0; i < newStr.length; i++){
-        if(newStr.substring(i, i+10)  === "**********"){ 
+        if(newStr.substring(i, [i+10])  === "**********"){ 
             decodeStr += "*";
+            i += 9;
         }
-        if(newStr[i] === " "){
+        else if(newStr[i] === " "){
             decodeStr += " ";
         }
-        if(newStr.substring(i, i+1) === "11"){
+        else if((newStr[i] + newStr[i+1]) === "11"){
             decodeStr += "-";
+            i += 1
         }
-        if(newStr.substring(i, i+1) === "10"){
+        else if((newStr[i] + newStr[i+1]) === "10"){
             decodeStr += ".";
+            i += 1
         }
     }
     
     // 4. Разбиваем на массив
     let decodeWord = decodeStr.split("*");
-    let letters = decodeWord.join('').split(" ")
-    // console.log(decodeWord)
-    // console.log(letters)
+    let letters = decodeWord.join('').split(" ");
 
     // 5. 
     const result = decodeWord.map(word => {
@@ -80,16 +79,16 @@ function decode(bits) {
         return letters.map(letter => MORSE_TABLE[letter]).join("");
     });
 
-    return result
+    return result.join(' ')
 }
 
-// console.log(decode("00000011110000000010"))
-console.log(decode("0000001111**********0000001111"))
-// console.log(decode("000000111100000000100000000000000000011110000000010"))
-// console.log(decode("00000011110000000010000000111000111000001110001110111000000000000111011101110001110111011100000000001110111"))
-console.log(decode("0000101110000011111100101110100010111010000000101000000011100000111110**********00001010100011101110000011111100101111100000000010**********000010101000111011100010101010000011111100001111110010111010"))
+// console.log(decode("00000011110000000010")) //me
+// console.log(decode("00000011110000001111**********00000011110000001110")) //mm mn
+// console.log(decode("0000001111 0000000010 0000001111 0000000010")) // meme
+// console.log(decode("0000101110000011111100101110100010111010000000101000000011100000111110**********00001010100011101110000011111100101111100000000010**********000010101000111011100010101010000011111100001111110010111010")) //"rolling scope school" 
+// console.log(decode("000000101100000011100000101010000010111100000000100000101110**********00001111110000001110**********000000001100101010100000000010**********00001010110010111010000000001100000010100000001111000000101100000000110000000010**********00111110110000101011000000001000001010100000000011000000101000001111110000001110**********00001111110010101110**********0010111010000000101000101011100000000010**********000000001100101010100000000010**********00001010110000001110000000101000101010110000000010000010111000001010100000000010**********000000101100000011100000111010**********0000000010001010101100000000100000101110001110111100000000110010101010000000101000000011100000111110**********00000010100000101010**********10101010111010111111")) //"answer on the ultimate question of life the universe and everything is 42"
 
 
-// module.exports = {
-//     decode
-// }
+module.exports = {
+    decode
+}
